c3c78aeb546920fcc95c2fb71ae822d22b0c36e0
apikkondang <apikkondang@gmail.com>
2021-01-21 20:54:37 +0700

    Update 01linkto.js

--- 01linkto.js
+++ 01linkto.js
@@ -1,37 +1,38 @@
 //linkProdukINDO18plus
 //START build link #GetURLlinkuntuk
-        var linkAN = linkuntuk;
+var linkAN = linkuntuk;
 
-        if(linkAN == "apik" || linkAN == "a001") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/APIK"+parameter
-        else if(linkAN == "trisno" || linkAN == "a002") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/TRISNO"+parameter
-        else if(linkAN == "bmg" || linkAN == "a003") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/BMG"+parameter
-        else if(linkAN == "nurdin" || linkAN == "a004") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/NURDIN"+parameter
-        else if(linkAN == "janhadi" || linkAN == "a005") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/JANHADI"+parameter
-        else if(linkAN == "rizki" || linkAN == "a006") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/RIZKI"+parameter
-        else if(linkAN == "wasmin" || linkAN == "a007") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/WASMIN"+parameter
-        else if(linkAN == "asep" || linkAN == "a008") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/ASEP"+parameter
-        else if(linkAN == "darsono" || linkAN == "a010") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/DARSONO"+parameter
-        else if(linkAN == "dirga" || linkAN == "a011") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/DIRGA"+parameter        
-        else if(linkAN == "ENDgakGROUP" || linkAN == "a009") linkTrackAN = "linkGAKend"+parameter
-        else if(linkAN == "jeni" || linkAN == "j001") linkTrackAN = "https://porrect-loculying.com/7cd8a0e8-efd2-4acb-9cf9-d4665cc1c3bb"+parameter            
-        else if(linkAN == "alkhalifi" || linkAN == "j002") linkTrackAN = "https://porrect-loculying.com/e691f605-58b1-4da2-a4ed-e4849d05872b"+parameter            
-        else if(linkAN == "indra" || linkAN == "j003") linkTrackAN = "https://porrect-loculying.com/80ad3d44-c21c-49a7-8825-e23605282c17"+parameter
-        else if(linkAN == "andi" || linkAN == "j004") linkTrackAN = "https://porrect-loculying.com/b3be7af8-3061-4d4c-8e12-705c573456ed"+parameter
-        else if(linkAN == "mawux" || linkAN == "j005") linkTrackAN = "https://porrect-loculying.com/f3c2c3d6-d5ba-41fb-993f-efac57a16b47"+parameter
-        else if(linkAN == "pongo" || linkAN == "j006") linkTrackAN = "https://porrect-loculying.com/0d4b0070-ea59-4fc0-b928-efcc3f2dd6af"+parameter
-        else if(linkAN == "aurora" || linkAN == "j007") linkTrackAN = "https://porrect-loculying.com/756bed7c-589c-4d86-bdc7-74a750783bce"+parameter
-        else if(linkAN == "aas" || linkAN == "j008") linkTrackAN = "https://porrect-loculying.com/7475f5ef-d54d-4b0c-98e0-27349b7d0e1e"+parameter
-        else if(linkAN == "irwan" || linkAN == "j009") linkTrackAN = "https://porrect-loculying.com/fb88838d-f7d0-4917-b944-c44c46554108"+parameter
-        else if(linkAN == "guh" || linkAN == "j0010") linkTrackAN = "https://porrect-loculying.com/0acb212f-a27b-488e-ae39-b56e2420f711"+parameter
-        else if(linkAN == "awal" || linkAN == "j0011") linkTrackAN = "https://porrect-loculying.com/fa0bce56-6081-48cf-bc46-5cba81166d53"+parameter
-        else if(linkAN == "rico" || linkAN == "j0012") linkTrackAN = "https://porrect-loculying.com/50b425a3-320d-45cd-894f-8414458107e3"+parameter
-        else if(linkAN == "izun" || linkAN == "j0013") linkTrackAN = "https://porrect-loculying.com/be5e45dd-02dd-43b6-a367-702f15e612aa"+parameter
-        else if(linkAN == "kirana" || linkAN == "j0014") linkTrackAN = "https://porrect-loculying.com/209cc71d-c03b-4f38-bf43-6fc28eb5c6ed"+parameter
-        else if(linkAN == "arsya" || linkAN == "j0015") linkTrackAN = "https://porrect-loculying.com/04eca168-b269-49a6-8d47-fab03abc7c32"+parameter
-        else if(linkAN == "ucup" || linkAN == "j0016") linkTrackAN = "https://porrect-loculying.com/15c2c4ec-c829-4e7c-b156-cad08139c125"+parameter
-        else if(linkAN == "danis" || linkAN == "j0017") linkTrackAN = "https://porrect-loculying.com/53f369b0-02b5-4634-baa4-f716a80ba117"+parameter
-        else if(linkAN == "humaira" || linkAN == "j0018") linkTrackAN = "https://porrect-loculying.com/d831ea2f-f40f-4142-8f06-d5371b5bed9c"+parameter
-        else if(linkAN == "toip" || linkAN == "j0019") linkTrackAN = "https://porrect-loculying.com/09ccf77d-c887-4066-84ef-de4dad53beea"+parameter
-        else if(linkAN == "NAMAorangBARUtanpaSpasiHURUFkecilSEMUA" || linkAN == "kode0000") linkTrackAN = "LINKtrackingORANGbaru"+parameter
-        else linkTrackAN = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"+parameter
+if(linkAN == "apik" || linkAN == "a001") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/APIK"+parameter
+else if(linkAN == "trisno" || linkAN == "a002") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/TRISNO"+parameter
+else if(linkAN == "bmg" || linkAN == "a003") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/BMG"+parameter
+else if(linkAN == "nurdin" || linkAN == "a004") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/NURDIN"+parameter
+else if(linkAN == "janhadi" || linkAN == "a005") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/JANHADI"+parameter
+else if(linkAN == "rizki" || linkAN == "a006") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/RIZKI"+parameter
+else if(linkAN == "wasmin" || linkAN == "a007") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/WASMIN"+parameter
+else if(linkAN == "asep" || linkAN == "a008") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/ASEP"+parameter
+else if(linkAN == "darsono" || linkAN == "a010") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/DARSONO"+parameter
+else if(linkAN == "dirga" || linkAN == "a011") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/DIRGA"+parameter        
+else if(linkAN == "iqbal" || linkAN == "a012") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/IQBAL"+parameter
+else if(linkAN == "ENDgakGROUP" || linkAN == "a009") linkTrackAN = "linkGAKend"+parameter
+else if(linkAN == "jeni" || linkAN == "j001") linkTrackAN = "https://porrect-loculying.com/7cd8a0e8-efd2-4acb-9cf9-d4665cc1c3bb"+parameter            
+else if(linkAN == "alkhalifi" || linkAN == "j002") linkTrackAN = "https://porrect-loculying.com/e691f605-58b1-4da2-a4ed-e4849d05872b"+parameter            
+else if(linkAN == "indra" || linkAN == "j003") linkTrackAN = "https://porrect-loculying.com/80ad3d44-c21c-49a7-8825-e23605282c17"+parameter
+else if(linkAN == "andi" || linkAN == "j004") linkTrackAN = "https://porrect-loculying.com/b3be7af8-3061-4d4c-8e12-705c573456ed"+parameter
+else if(linkAN == "mawux" || linkAN == "j005") linkTrackAN = "https://porrect-loculying.com/f3c2c3d6-d5ba-41fb-993f-efac57a16b47"+parameter
+else if(linkAN == "pongo" || linkAN == "j006") linkTrackAN = "https://porrect-loculying.com/0d4b0070-ea59-4fc0-b928-efcc3f2dd6af"+parameter
+else if(linkAN == "aurora" || linkAN == "j007") linkTrackAN = "https://porrect-loculying.com/756bed7c-589c-4d86-bdc7-74a750783bce"+parameter
+else if(linkAN == "aas" || linkAN == "j008") linkTrackAN = "https://porrect-loculying.com/7475f5ef-d54d-4b0c-98e0-27349b7d0e1e"+parameter
+else if(linkAN == "irwan" || linkAN == "j009") linkTrackAN = "https://porrect-loculying.com/fb88838d-f7d0-4917-b944-c44c46554108"+parameter
+else if(linkAN == "guh" || linkAN == "j0010") linkTrackAN = "https://porrect-loculying.com/0acb212f-a27b-488e-ae39-b56e2420f711"+parameter
+else if(linkAN == "awal" || linkAN == "j0011") linkTrackAN = "https://porrect-loculying.com/fa0bce56-6081-48cf-bc46-5cba81166d53"+parameter
+else if(linkAN == "rico" || linkAN == "j0012") linkTrackAN = "https://porrect-loculying.com/50b425a3-320d-45cd-894f-8414458107e3"+parameter
+else if(linkAN == "izun" || linkAN == "j0013") linkTrackAN = "https://porrect-loculying.com/be5e45dd-02dd-43b6-a367-702f15e612aa"+parameter
+else if(linkAN == "kirana" || linkAN == "j0014") linkTrackAN = "https://porrect-loculying.com/209cc71d-c03b-4f38-bf43-6fc28eb5c6ed"+parameter
+else if(linkAN == "arsya" || linkAN == "j0015") linkTrackAN = "https://porrect-loculying.com/04eca168-b269-49a6-8d47-fab03abc7c32"+parameter
+else if(linkAN == "ucup" || linkAN == "j0016") linkTrackAN = "https://porrect-loculying.com/15c2c4ec-c829-4e7c-b156-cad08139c125"+parameter
+else if(linkAN == "danis" || linkAN == "j0017") linkTrackAN = "https://porrect-loculying.com/53f369b0-02b5-4634-baa4-f716a80ba117"+parameter
+else if(linkAN == "humaira" || linkAN == "j0018") linkTrackAN = "https://porrect-loculying.com/d831ea2f-f40f-4142-8f06-d5371b5bed9c"+parameter
+else if(linkAN == "toip" || linkAN == "j0019") linkTrackAN = "https://porrect-loculying.com/09ccf77d-c887-4066-84ef-de4dad53beea"+parameter
+else if(linkAN == "NAMAorangBARUtanpaSpasiHURUFkecilSEMUA" || linkAN == "kode0000") linkTrackAN = "LINKtrackingORANGbaru"+parameter
+else linkTrackAN = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"+parameter
 //END build link #GetURLlinkuntuk
