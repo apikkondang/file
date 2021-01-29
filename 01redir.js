8cdaf908a76ae91cd6fd41024607044c6ef4f582
apikkondang <apikkondang@gmail.com>
2021-01-30 06:50:32 +0700

    Update 01redir.js

--- 01redir.js
+++ 01redir.js
@@ -2,7 +2,7 @@ var bhs = lang;
 	if(bhs == "ID") 	var linkredir = "https://www.blog-hot.com/p/blog-page_24.html"  
 	else if(bhs == "MY") 	var linkredir = "https://www.blog-hot.com/p/my-penjelasan-fxc.html"          
 	else if(bhs == "IN") 	var linkredir = "https://www.blog-hot.com/p/in.html"          
-	else if(bhs == "TH") 	var linkredir = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"          
+	else if(bhs == "TH") 	var linkredir = "http://bit.ly/2MDtdmw"          
 	else if(bhs == "VN") 	var linkredir = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"          
 	else if(bhs == "TR") 	var linkredir = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"    
 	else if(bhs == "RO") 	var linkredir = "https://porrect-loculying.com/791c5ae0-e222-4f8c-bd77-d6200b760d6b"    
