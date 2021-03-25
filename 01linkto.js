641479b9e07697e58cb9f0aba012fb9cef7b116d
apikkondang <apikkondang@gmail.com>
2021-03-26 01:02:43 +0700

    Update 01linkto.js

--- 01linkto.js
+++ 01linkto.js
@@ -1,40 +1,42 @@
-//linkProdukINDO18plus
 //START build link #GetURLlinkuntuk
 var linkAN = linkuntuk;
+var linkTR = "https://dashboard.sporadise.com"
 
-if(linkAN == "apik" || linkAN == "a001") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/APIK"+parameter
-else if(linkAN == "trisno" || linkAN == "a002") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/TRISNO"+parameter
-else if(linkAN == "bmg" || linkAN == "a003") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/BMG"+parameter
-else if(linkAN == "nurdin" || linkAN == "a004") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/NURDIN"+parameter
-else if(linkAN == "janhadi" || linkAN == "a005") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/JANHADI"+parameter
-else if(linkAN == "rizki" || linkAN == "a006") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/RIZKI"+parameter
-else if(linkAN == "wasmin" || linkAN == "a007") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/WASMIN"+parameter
-else if(linkAN == "asep" || linkAN == "a008") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/ASEP"+parameter
-else if(linkAN == "ciki" || linkAN == "a008a") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/CIKI/ASEP"+parameter
-else if(linkAN == "sihaloho" || linkAN == "a009") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/SIHALOHO"+parameter
-else if(linkAN == "darsono" || linkAN == "a010") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/DARSONO"+parameter
-else if(linkAN == "dirga" || linkAN == "a011") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/DIRGA"+parameter        
-else if(linkAN == "iqbal" || linkAN == "a012") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/IQBAL"+parameter
-else if(linkAN == "lani" || linkAN == "a013") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/LANI"+parameter
-else if(linkAN == "indra" || linkAN == "a014") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/INDRA"+parameter
-else if(linkAN == "rio" || linkAN == "a015") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/RIO"+parameter
-else if(linkAN == "dodi" || linkAN == "a016") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/DODI"+parameter
-else if(linkAN == "arif" || linkAN == "a017") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/ARIF"+parameter
-else if(linkAN == "bagus" || linkAN == "a018") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/BAGUS"+parameter
-else if(linkAN == "verel" || linkAN == "a019") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/VEREL"+parameter
-else if(linkAN == "maman" || linkAN == "a020") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/MAMAN"+parameter
-else if(linkAN == "dudi" || linkAN == "a021") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/DUDI"+parameter
-else if(linkAN == "endra" || linkAN == "a022") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/ENDRA"+parameter
-else if(linkAN == "wanda" || linkAN == "a023") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/WANDA"+parameter
-else if(linkAN == "sigit" || linkAN == "a024") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/SIGIT"+parameter
-else if(linkAN == "sendi" || linkAN == "a025") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/SENDI"+parameter
-else if(linkAN == "karisa" || linkAN == "a026") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/KARISA"+parameter
-else if(linkAN == "tito" || linkAN == "a027") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/TITO"+parameter
-else if(linkAN == "andi" || linkAN == "a028") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/ANDI"+parameter
-else if(linkAN == "dita" || linkAN == "a029") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/DITA"+parameter
-else if(linkAN == "medi" || linkAN == "a030") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/MEDI"+parameter
-else if(linkAN == "cahyudin" || linkAN == "a031") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/CAHYUDIN"+parameter
-else if(linkAN == "tiko" || linkAN == "a032") linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/TIKO"+parameter
-else if(linkAN == "NAMAorangBARUtanpaSpasiHURUFkecilSEMUA" || linkAN == "kode0000") linkTrackAN = "LINKtrackingORANGbaru"+parameter
-else linkTrackAN = "https://dashboard.sporadise.com/track/smartlink/source/NYASAR"+parameter
+if(lander == "kuis") linkTrackAN = 'https://storemaan22.easy.co/?linkuntuk='+linkuntuk+'&pixel='+pixel+'&lang='+lang+'&produk='+produk+'&subacc='+img+'&subacc2='+account+'&subacc3='+adcopy+'&subacc4=dirQUISyes&subacc5='+urlcustomfile+'&subacc6='+tanggal+'&plp='+plp
+else if(linkAN  == "apik"       || linkAN == "a001")    linkTrackAN = linkTR+"/track/smartlink/source/APIK"+parameter
+else if(linkAN  == "trisno"     || linkAN == "a002")    linkTrackAN = linkTR+"/track/smartlink/source/TRISNO"+parameter
+else if(linkAN  == "bmg"        || linkAN == "a003")    linkTrackAN = linkTR+"/track/smartlink/source/BMG"+parameter
+else if(linkAN  == "nurdin"     || linkAN == "a004")    linkTrackAN = linkTR+"/track/smartlink/source/NURDIN"+parameter
+else if(linkAN  == "janhadi"    || linkAN == "a005")    linkTrackAN = linkTR+"/track/smartlink/source/JANHADI"+parameter
+else if(linkAN  == "rizki"      || linkAN == "a006")    linkTrackAN = linkTR+"/track/smartlink/source/RIZKI"+parameter
+else if(linkAN  == "wasmin"     || linkAN == "a007")    linkTrackAN = linkTR+"/track/smartlink/source/WASMIN"+parameter
+else if(linkAN  == "asep"       || linkAN == "a008")    linkTrackAN = linkTR+"/track/smartlink/source/ASEP"+parameter
+else if(linkAN  == "ciki"       || linkAN == "a008a")   linkTrackAN = linkTR+"/track/smartlink/CIKI/ASEP"+parameter
+else if(linkAN  == "sihaloho"   || linkAN == "a009")    linkTrackAN = linkTR+"/track/smartlink/source/SIHALOHO"+parameter
+else if(linkAN  == "darsono"    || linkAN == "a010")    linkTrackAN = linkTR+"/track/smartlink/source/DARSONO"+parameter
+else if(linkAN  == "dirga"      || linkAN == "a011")    linkTrackAN = linkTR+"/track/smartlink/source/DIRGA"+parameter        
+else if(linkAN  == "iqbal"      || linkAN == "a012")    linkTrackAN = linkTR+"/track/smartlink/source/IQBAL"+parameter
+else if(linkAN  == "lani"       || linkAN == "a013")    linkTrackAN = linkTR+"/track/smartlink/source/LANI"+parameter
+else if(linkAN  == "indra"      || linkAN == "a014")    linkTrackAN = linkTR+"/track/smartlink/source/INDRA"+parameter
+else if(linkAN  == "rio"        || linkAN == "a015")    linkTrackAN = linkTR+"/track/smartlink/source/RIO"+parameter
+else if(linkAN  == "dodi"       || linkAN == "a016")    linkTrackAN = linkTR+"/track/smartlink/source/DODI"+parameter
+else if(linkAN  == "arif"       || linkAN == "a017")    linkTrackAN = linkTR+"/track/smartlink/source/ARIF"+parameter
+else if(linkAN  == "bagus"      || linkAN == "a018")    linkTrackAN = linkTR+"/track/smartlink/source/BAGUS"+parameter
+else if(linkAN  == "verel"      || linkAN == "a019")    linkTrackAN = linkTR+"/track/smartlink/source/VEREL"+parameter
+else if(linkAN  == "maman"      || linkAN == "a020")    linkTrackAN = linkTR+"/track/smartlink/source/MAMAN"+parameter
+else if(linkAN  == "dudi"       || linkAN == "a021")    linkTrackAN = linkTR+"/track/smartlink/source/DUDI"+parameter
+else if(linkAN  == "endra"      || linkAN == "a022")    linkTrackAN = linkTR+"/track/smartlink/source/ENDRA"+parameter
+else if(linkAN  == "wanda"      || linkAN == "a023")    linkTrackAN = linkTR+"/track/smartlink/source/WANDA"+parameter
+else if(linkAN  == "sigit"      || linkAN == "a024")    linkTrackAN = linkTR+"/track/smartlink/source/SIGIT"+parameter
+else if(linkAN  == "sendi"      || linkAN == "a025")    linkTrackAN = linkTR+"/track/smartlink/source/SENDI"+parameter
+else if(linkAN  == "karisa"     || linkAN == "a026")    linkTrackAN = linkTR+"/track/smartlink/source/KARISA"+parameter
+else if(linkAN  == "tito"       || linkAN == "a027")    linkTrackAN = linkTR+"/track/smartlink/source/TITO"+parameter
+else if(linkAN  == "andi"       || linkAN == "a028")    linkTrackAN = linkTR+"/track/smartlink/source/ANDI"+parameter
+else if(linkAN  == "dita"       || linkAN == "a029")    linkTrackAN = linkTR+"/track/smartlink/source/DITA"+parameter
+else if(linkAN  == "medi"       || linkAN == "a030")    linkTrackAN = linkTR+"/track/smartlink/source/MEDI"+parameter
+else if(linkAN  == "cahyudin"   || linkAN == "a031")    linkTrackAN = linkTR+"/track/smartlink/source/CAHYUDIN"+parameter
+else if(linkAN  == "tiko"       || linkAN == "a032")    linkTrackAN = linkTR+"/track/smartlink/source/TIKO"+parameter
+else linkTrackAN = linkTR+"/track/smartlink/source/NYASAR"+parameter
 //END build link #GetURLlinkuntuk
+
+
