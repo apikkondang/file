9911cd570213fe838c327cbf40dff72decd6114f
apikkondang <apikkondang@gmail.com>
2021-03-31 21:45:43 +0700

    Update 01linkto.js

--- 01linkto.js
+++ 01linkto.js
@@ -1,42 +1,41 @@
 //START build link #GetURLlinkuntuk
 var linkAN = linkuntuk;
-var linkTR = "https://dashboard.sporadise.com"
+var linkTR = "https://dashboard.sporadise.com/track/smartlink"
 
 if(lander == "kuis") linkTrackAN = 'https://www.technolect.org/?linkuntuk='+linkuntuk+'&pixel='+pixel+'&lang='+lang+'&produk='+produk+'&subacc='+img+'&subacc2='+account+'&subacc3='+adcopy+'&subacc4=dirQUISyes&subacc5='+urlcustomfile+'&subacc6='+tanggal+'&plp='+plp
-else if(linkAN  == "apik"       || linkAN == "a001")    linkTrackAN = linkTR+"/track/smartlink/source/APIK"+parameter
-else if(linkAN  == "trisno"     || linkAN == "a002")    linkTrackAN = linkTR+"/track/smartlink/source/TRISNO"+parameter
-else if(linkAN  == "bmg"        || linkAN == "a003")    linkTrackAN = linkTR+"/track/smartlink/source/BMG"+parameter
-else if(linkAN  == "nurdin"     || linkAN == "a004")    linkTrackAN = linkTR+"/track/smartlink/source/NURDIN"+parameter
-else if(linkAN  == "janhadi"    || linkAN == "a005")    linkTrackAN = linkTR+"/track/smartlink/source/JANHADI"+parameter
-else if(linkAN  == "rizki"      || linkAN == "a006")    linkTrackAN = linkTR+"/track/smartlink/source/RIZKI"+parameter
-else if(linkAN  == "wasmin"     || linkAN == "a007")    linkTrackAN = linkTR+"/track/smartlink/source/WASMIN"+parameter
-else if(linkAN  == "asep"       || linkAN == "a008")    linkTrackAN = linkTR+"/track/smartlink/source/ASEP"+parameter
-else if(linkAN  == "ciki"       || linkAN == "a008a")   linkTrackAN = linkTR+"/track/smartlink/CIKI/ASEP"+parameter
-else if(linkAN  == "sihaloho"   || linkAN == "a009")    linkTrackAN = linkTR+"/track/smartlink/source/SIHALOHO"+parameter
-else if(linkAN  == "darsono"    || linkAN == "a010")    linkTrackAN = linkTR+"/track/smartlink/source/DARSONO"+parameter
-else if(linkAN  == "dirga"      || linkAN == "a011")    linkTrackAN = linkTR+"/track/smartlink/source/DIRGA"+parameter        
-else if(linkAN  == "iqbal"      || linkAN == "a012")    linkTrackAN = linkTR+"/track/smartlink/source/IQBAL"+parameter
-else if(linkAN  == "lani"       || linkAN == "a013")    linkTrackAN = linkTR+"/track/smartlink/source/LANI"+parameter
-else if(linkAN  == "indra"      || linkAN == "a014")    linkTrackAN = linkTR+"/track/smartlink/source/INDRA"+parameter
-else if(linkAN  == "rio"        || linkAN == "a015")    linkTrackAN = linkTR+"/track/smartlink/source/RIO"+parameter
-else if(linkAN  == "dodi"       || linkAN == "a016")    linkTrackAN = linkTR+"/track/smartlink/source/DODI"+parameter
-else if(linkAN  == "arif"       || linkAN == "a017")    linkTrackAN = linkTR+"/track/smartlink/source/ARIF"+parameter
-else if(linkAN  == "bagus"      || linkAN == "a018")    linkTrackAN = linkTR+"/track/smartlink/source/BAGUS"+parameter
-else if(linkAN  == "verel"      || linkAN == "a019")    linkTrackAN = linkTR+"/track/smartlink/source/VEREL"+parameter
-else if(linkAN  == "maman"      || linkAN == "a020")    linkTrackAN = linkTR+"/track/smartlink/source/MAMAN"+parameter
-else if(linkAN  == "dudi"       || linkAN == "a021")    linkTrackAN = linkTR+"/track/smartlink/source/DUDI"+parameter
-else if(linkAN  == "endra"      || linkAN == "a022")    linkTrackAN = linkTR+"/track/smartlink/source/ENDRA"+parameter
-else if(linkAN  == "wanda"      || linkAN == "a023")    linkTrackAN = linkTR+"/track/smartlink/source/WANDA"+parameter
-else if(linkAN  == "sigit"      || linkAN == "a024")    linkTrackAN = linkTR+"/track/smartlink/source/SIGIT"+parameter
-else if(linkAN  == "sendi"      || linkAN == "a025")    linkTrackAN = linkTR+"/track/smartlink/source/SENDI"+parameter
-else if(linkAN  == "karisa"     || linkAN == "a026")    linkTrackAN = linkTR+"/track/smartlink/source/KARISA"+parameter
-else if(linkAN  == "tito"       || linkAN == "a027")    linkTrackAN = linkTR+"/track/smartlink/source/TITO"+parameter
-else if(linkAN  == "andi"       || linkAN == "a028")    linkTrackAN = linkTR+"/track/smartlink/source/ANDI"+parameter
-else if(linkAN  == "dita"       || linkAN == "a029")    linkTrackAN = linkTR+"/track/smartlink/source/DITA"+parameter
-else if(linkAN  == "medi"       || linkAN == "a030")    linkTrackAN = linkTR+"/track/smartlink/source/MEDI"+parameter
-else if(linkAN  == "cahyudin"   || linkAN == "a031")    linkTrackAN = linkTR+"/track/smartlink/source/CAHYUDIN"+parameter
-else if(linkAN  == "tiko"       || linkAN == "a032")    linkTrackAN = linkTR+"/track/smartlink/source/TIKO"+parameter
-else linkTrackAN = linkTR+"/track/smartlink/source/NYASAR"+parameter
+else if(linkAN  == "apik"       || linkAN == "a001")    linkTrackAN = linkTR+"/source/APIK"+parameter
+else if(linkAN  == "trisno"     || linkAN == "a002")    linkTrackAN = linkTR+"/source/TRISNO"+parameter
+else if(linkAN  == "bmg"        || linkAN == "a003")    linkTrackAN = linkTR+"/source/BMG"+parameter
+else if(linkAN  == "nurdin"     || linkAN == "a004")    linkTrackAN = linkTR+"/source/NURDIN"+parameter
+else if(linkAN  == "janhadi"    || linkAN == "a005")    linkTrackAN = linkTR+"/source/JANHADI"+parameter
+else if(linkAN  == "rizki"      || linkAN == "a006")    linkTrackAN = linkTR+"/source/RIZKI"+parameter
+else if(linkAN  == "wasmin"     || linkAN == "a007")    linkTrackAN = linkTR+"/source/WASMIN"+parameter
+else if(linkAN  == "asep"       || linkAN == "a008")    linkTrackAN = linkTR+"/source/ASEP"+parameter
+else if(linkAN  == "ciki"       || linkAN == "a008a")   linkTrackAN = linkTR+"/CIKI/ASEP"+parameter
+else if(linkAN  == "sihaloho"   || linkAN == "a009")    linkTrackAN = linkTR+"/source/SIHALOHO"+parameter
+else if(linkAN  == "darsono"    || linkAN == "a010")    linkTrackAN = linkTR+"/source/DARSONO"+parameter
+else if(linkAN  == "dirga"      || linkAN == "a011")    linkTrackAN = linkTR+"/source/DIRGA"+parameter        
+else if(linkAN  == "iqbal"      || linkAN == "a012")    linkTrackAN = linkTR+"/source/IQBAL"+parameter
+else if(linkAN  == "lani"       || linkAN == "a013")    linkTrackAN = linkTR+"/source/LANI"+parameter
+else if(linkAN  == "indra"      || linkAN == "a014")    linkTrackAN = linkTR+"/source/INDRA"+parameter
+else if(linkAN  == "rio"        || linkAN == "a015")    linkTrackAN = linkTR+"/source/RIO"+parameter
+else if(linkAN  == "dodi"       || linkAN == "a016")    linkTrackAN = linkTR+"/source/DODI"+parameter
+else if(linkAN  == "arif"       || linkAN == "a017")    linkTrackAN = linkTR+"/source/ARIF"+parameter
+else if(linkAN  == "bagus"      || linkAN == "a018")    linkTrackAN = linkTR+"/source/BAGUS"+parameter
+else if(linkAN  == "verel"      || linkAN == "a019")    linkTrackAN = linkTR+"/source/VEREL"+parameter
+else if(linkAN  == "maman"      || linkAN == "a020")    linkTrackAN = linkTR+"/source/MAMAN"+parameter
+else if(linkAN  == "dudi"       || linkAN == "a021")    linkTrackAN = linkTR+"/source/DUDI"+parameter
+else if(linkAN  == "endra"      || linkAN == "a022")    linkTrackAN = linkTR+"/source/ENDRA"+parameter
+else if(linkAN  == "wanda"      || linkAN == "a023")    linkTrackAN = linkTR+"/source/WANDA"+parameter
+else if(linkAN  == "sigit"      || linkAN == "a024")    linkTrackAN = linkTR+"/source/SIGIT"+parameter
+else if(linkAN  == "sendi"      || linkAN == "a025")    linkTrackAN = linkTR+"/source/SENDI"+parameter
+else if(linkAN  == "karisa"     || linkAN == "a026")    linkTrackAN = linkTR+"/source/KARISA"+parameter
+else if(linkAN  == "tito"       || linkAN == "a027")    linkTrackAN = linkTR+"/source/TITO"+parameter
+else if(linkAN  == "andi"       || linkAN == "a028")    linkTrackAN = linkTR+"/source/ANDI"+parameter
+else if(linkAN  == "dita"       || linkAN == "a029")    linkTrackAN = linkTR+"/source/DITA"+parameter
+else if(linkAN  == "medi"       || linkAN == "a030")    linkTrackAN = linkTR+"/source/MEDI"+parameter
+else if(linkAN  == "cahyudin"   || linkAN == "a031")    linkTrackAN = linkTR+"/source/CAHYUDIN"+parameter
+else if(linkAN  == "tiko"       || linkAN == "a032")    linkTrackAN = linkTR+"/source/TIKO"+parameter
+else if(linkAN  == "edi"        || linkAN == "a033")    linkTrackAN = linkTR+"/source/EDI"+parameter
+else linkTrackAN = linkTR+"/source/NYASAR"+parameter
 //END build link #GetURLlinkuntuk
-
-
