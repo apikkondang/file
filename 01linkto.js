2a2bdcede492ee393d458144b72c8d0ef3cea11f
apikkondang <apikkondang@gmail.com>
2021-05-09 13:16:20 +0700

    Update 01linkto.js

--- 01linkto.js
+++ 01linkto.js
@@ -42,3 +42,15 @@ else if(linkAN  == "edi"        || linkAN == "a033")    linkTrackAN = linkTR+"/s
 else if(linkAN  == null || linkAN  == "" || linkAN  == "null" || linkAN  == "NULL")  linkTrackAN = linkTR+"/source/NYASAR"+param
 else linkTrackAN = linkTR+"/source/"+linkAN.toUpperCase()+param
 //END build link #GetURLlinkuntuk
+
+
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', pixel);
+fbq('track', 'PageView');
